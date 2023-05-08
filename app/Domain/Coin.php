9aa0ad6820e9d79e02fc38793d3491b1e77c5ac4
Álvaro Larraya <alvaro.larraya1@gmail.com>
2023-05-08 11:51:05 +0000

    [2] - problems fixed

--- app/Domain/Coin.php
+++ app/Domain/Coin.php
@@ -4,35 +4,19 @@ namespace App\Domain;
 
 class User
 {
-    private int $id;
-    private string $email;
-
-    private string $coin_id;
+    private string $coinId;
     private string $name;
     private string $symbol;
     private float $amount;
-    private float $value_usd;
+    private float $valueUsd;
 
-    /**
-     * @param int $id
-     * @param string $email
-     * @param string $coinId
-     * @param string $name
-     * @param string $symbol
-     * @param float $amount
-     * @param float $valueUsd
-     */
     public function __construct(
-        int $id,
-        string $email,
         string $coinId,
         string $name,
         string $symbol,
         float $amount,
         float $valueUsd
     ) {
-        $this->id = $id;
-        $this->email = $email;
         $this->coin_id = $coinId;
         $this->name = $name;
         $this->symbol = $symbol;
