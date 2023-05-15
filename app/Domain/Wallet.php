4d7c8f54c7c549e7d9afdb9d843f547977305e36
Alvaro Larraya <74350895+alvarolarraya12@users.noreply.github.com>
2023-05-16 00:05:01 +0200

    Update Wallet.php

--- app/Domain/Wallet.php
+++ app/Domain/Wallet.php
@@ -4,17 +4,17 @@ namespace App\Domain;
 
 class Wallet
 {
-    private int $walleId;
+    private int $walletId;
     private array $coins;
     private int $balance;
 
     public function __construct($walletId)
     {
-        $this->walleId = $walletId;
+        $this->walletId = $walletId;
     }
 
     public function getWalletId(): string
     {
-        return $this->walleId;
+        return $this->walletId;
     }
 }
